c2e7420b153c69f30bfc8d4847015b0dc174ec61
Lukas Hejnal <hejnaluk@fel.cvut.cz>
2019-01-27 22:05:53 +0100

    Add assertAll example

--- src/test/java/junit5/AssertionsTest.java
+++ src/test/java/junit5/AssertionsTest.java
@@ -2,7 +2,10 @@ package junit5;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 public class AssertionsTest {
 
@@ -16,4 +19,11 @@ public class AssertionsTest {
         assertEquals(expected, result, delta, () -> "Delta is too low :)");
     }
 
+    @Test
+    public void testAssertAll() {
+        assertAll( "Tests",
+                () -> {},
+                () -> assertEquals("", "")
+        );
+    }
 }
