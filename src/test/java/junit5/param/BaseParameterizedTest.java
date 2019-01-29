9ecc2b8f0a6af739a8a5b98f60f06679ae8b8a67
Lukas Hejnal <hejnaluk@fel.cvut.cz>
2019-01-29 22:24:46 +0100

    Add simple parameterized test, FYI CsvSource does not work

---
+++ src/test/java/junit5/param/BaseParameterizedTest.java
@@ -0,0 +1,66 @@
+package junit5.param;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Basic parameterized test")
+public class BaseParameterizedTest {
+
+    @ParameterizedTest()
+    @ValueSource( ints = { 1, 2, 3, 10, 11, 12, 15})
+    void testParameterizedIntegers( final int number) {
+        assertTrue(number < 20);
+    }
+
+    @ParameterizedTest()
+    @EnumSource( TimeUnit.class)
+    void testAllEnumValues( TimeUnit unit) {
+        System.out.println(unit.toString());
+    }
+
+    @ParameterizedTest(name="Test prime number \"{0}\"")
+    @MethodSource("generatePrimeNumbers")
+    void testPrimeNumbers(final int number) {
+        assertTrue ( BigInteger.valueOf(number).isProbablePrime(1), "Number " + number + " is prime number");
+    }
+
+
+    static Collection<Integer> generatePrimeNumbers() {
+        return List.of(3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37);
+    }
+
+    @ParameterizedTest(name="Name test first name is \"{0}\", last name is \"{1}\"")
+    @MethodSource("generateNames")
+    void testNamesMethodSource(String firstName, String lastName) {
+        assertTrue(!firstName.isBlank());
+        assertTrue(!lastName.isBlank());
+    }
+
+    static Stream<Arguments> generateNames() {
+        return Stream.of(
+                Arguments.of ( "A", "B"),
+                Arguments.of ( "C", "D"),
+                Arguments.of ( "E", "F")
+        );
+    }
+
+
+//  Highle experimental feature :)
+//    @ParameterizedTest(name="CSV parameter test")
+//    @CsvSource( value = {"A", "B", "C", "D", "E", "F", "G", "H"})
+//    void testNamesCsv(String firstName, String lastName) {
+//        assertTrue(!firstName.isBlank());
+//        assertTrue(!lastName.isBlank());
+//    }
+}
