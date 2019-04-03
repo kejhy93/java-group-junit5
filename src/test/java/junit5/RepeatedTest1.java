8867a88b5b4d6f91a7273c87e8b594881ba80070
Lukáš Hejnal <kejhy93@gmail.com>
2019-04-03 12:54:11 +0200

    Fix conflift

--- src/test/java/junit5/RepeatedTest1.java
+++ src/test/java/junit5/RepeatedTest1.java
@@ -1,17 +1,18 @@
 package junit5;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RepeatedTest {
+public class RepeatedTest1 {
 
-    @RepeatedTest(value = 1, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @RepeatedTest(value = 10, name = "{displayName} {currentRepetition}/{totalRepetitions}")
     @DisplayName("Repeat!")
-    void customDisplayName(TestInfo testInfo) {
-        assertEquals("Repeat! 1/1", testInfo.getDisplayName());
+    void customDisplayName() {
+        int res = 0;
+        assertEquals(0, res);
     }
 
 }
