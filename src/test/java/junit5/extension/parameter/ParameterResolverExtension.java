673267694b76555a87deec7cd2864880406414a4
Lukáš Hejnal <kejhy93@gmail.com>
2019-04-03 12:56:10 +0200

    Improve loggging level :)

--- src/test/java/junit5/extension/parameter/ParameterResolverExtension.java
+++ src/test/java/junit5/extension/parameter/ParameterResolverExtension.java
@@ -10,11 +10,13 @@ import java.util.Random;
 public class ParameterResolverExtension implements ParameterResolver {
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        System.out.println("Is parameter annotated with @RandomParam for test " + extensionContext.getDisplayName());
         return parameterContext.getParameter().isAnnotationPresent(RandomParam.class);
     }
 
     @Override
     public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        System.out.println("Annotated parameter @RandomParam found for test " + extensionContext.getDisplayName());
         return "Result=" + new Random().nextInt() + ".";
     }
 }
